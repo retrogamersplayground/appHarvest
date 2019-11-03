cbb24b2ceed5420f2657ddc74f7c14f0d192b0bc
Neil Floyd <neilfloyd99@gmail.com>
2019-11-03 11:12:21 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,91 @@
+let p = document.querySelector('.pLi p');
+console.log(p);
+
+let p2 = document.querySelector('.pLi2 p');
+console.log(p2);
+
+let p3 = document.querySelector('.pLi3 p');
+console.log(p3);
+
+let p4 = document.querySelector('.pLi4 p');
+console.log(p4);
+
+let p5 = document.querySelector('.pLi5 p');
+console.log(p5);
+
+
+
+
+function displayFunction() {
+    p.style.display = 'block';
+    p.style.position = 'absolute';
+    p.style.top = '1200px';
+    p.style.left = '0px';
+    p.style.backgroundColor = 'rgba(0,0,0,0.9)';
+    p.style.padding = '100px';
+    p.style.color = '#ffffff';
+}
+
+function hideFunction() {
+    p.style.display = 'none';
+}
+
+
+function display2Function() {
+    p2.style.display = 'block';
+    p2.style.position = 'absolute';
+    p2.style.top = '1200px';
+    p2.style.left = '0';
+    p2.style.backgroundColor = 'rgba(0,0,0,0.9)';
+    p2.style.padding = '100px';
+    p2.style.color = '#ffffff';
+}
+
+function hide2Function() {
+    p2.style.display = 'none';
+}
+
+
+function display3Function() {
+    p3.style.display = 'block';
+    p3.style.position = 'absolute';
+    p3.style.top = '1200px';
+    p3.style.left = '0px';
+    p3.style.backgroundColor = 'rgba(0,0,0,0.9)';
+    p3.style.padding = '100px';
+    p3.style.color = '#ffffff';
+}
+
+function hide3Function() {
+    p3.style.display = 'none';
+}
+
+
+function display4Function() {
+    p4.style.display = 'block';
+    p4.style.position = 'absolute';
+    p4.style.top = '900px';
+    p4.style.left = '0px';
+    p4.style.backgroundColor = 'rgba(0,0,0,0.9)';
+    p4.style.padding = '100px';
+    p4.style.color = '#ffffff';
+}
+
+function hide4Function() {
+    p4.style.display = 'none';
+}
+
+
+function display5Function() {
+    p5.style.display = 'block';
+    p5.style.position = 'absolute';
+    p5.style.top = '900px';
+    p5.style.left = '0px';
+    p5.style.backgroundColor = 'rgba(0,0,0,0.9)';
+    p5.style.padding = '100px';
+    p5.style.color = '#ffffff';
+}
+
+function hide5Function() {
+    p5.style.display = 'none';
+}
